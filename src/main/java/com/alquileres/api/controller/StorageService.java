ddd66908a4d897c1f038913f8da61e0c5a571125
Jose Soto <jose@xoolix.com>
2019-10-21 09:39:33 -0300

    remove unused imports

--- src/main/java/com/alquileres/api/controller/StorageService.java
+++ src/main/java/com/alquileres/api/controller/StorageService.java
@@ -1,11 +1,6 @@
 package com.alquileres.api.controller;
 
-
-import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.nio.file.Path;
-import java.util.stream.Stream;
 
 public interface StorageService {
 
--- src/main/java/com/alquileres/api/controller/StorageServiceImpl.java
+++ src/main/java/com/alquileres/api/controller/StorageServiceImpl.java
@@ -1,6 +1,5 @@
 package com.alquileres.api.controller;
 
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
